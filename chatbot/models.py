8898713e76e8d9b80077bf6ce7bbc0a7110f354c
jagritisrivastava <jagritisrvstv@gmail.com>
2026-05-24 21:18:26 +0530

    2nd commit

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ChatHistory(models.Model):
+    user_message = models.TextField()
+    bot_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_message
+
+
--- mentalhealth/settings.py
+++ mentalhealth/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'mentalhealth.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+   'default': dj_database_url.config(
+       default=os.environ.get('DATABASE_URL')
+   )
 }
+
 
 
 # Password validation
